7b86aa627d7a8d3a816c4c912ad85a3d9675711f
noor <noor.bakrieh@gmail.com>
2020-11-02 11:42:46 +0200

    grid fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 
 import './App.css';
 import FormPageOne from './FormOne';
+import {Form} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 
 
 
@@ -8,7 +13,13 @@ import FormPageOne from './FormOne';
 function App() {
   return (
     <div className="App">
-      <FormPageOne />
+      <Container>
+        <Row >
+          <Col xs={1}></Col>
+          <Col> <FormPageOne /> </Col>
+          <Col xs={1}></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
